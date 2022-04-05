f9f47a3a30f5a1f674254d8d1e1c0b56437545e4
Niicholai <derek.folds@gmail.com>
2022-04-05 18:12:27 -0500

    Changed line length & added template literals

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function diceRoll() {
   var roll1 = Math.floor(Math.random() * 6) + 1; //1-6
   var image1 = document.getElementById("img1");
-  image1.setAttribute("src", "images/dice" + roll1 + ".png"); //change player one dice image to 1-6
+  image1.setAttribute("src", "images/dice${roll1}.png"); //change player one dice image to 1-6
 
   var roll2 = Math.floor(Math.random() * 6) + 1; //1-6
   var image2 = document.getElementById("img2");
@@ -10,11 +10,9 @@ function diceRoll() {
 
   //winner results text
   if (roll1 > roll2) {
-    document.querySelector("h1").innerHTML =
-      "<img src='images/flagLeft.png'></img> Player 1 wins!";
+    document.querySelector("h1").innerHTML = "<img src='images/flagLeft.png'></img> Player 1 wins!";
   } else if (roll1 < roll2) {
-    document.querySelector("h1").innerHTML =
-      "Player 2 wins! <img src='images/flagRight.png'></img>";
+    document.querySelector("h1").innerHTML = "Player 2 wins! <img src='images/flagRight.png'></img>";
   } else {
     document.querySelector("h1").innerHTML =
       "<img src='images/flagLeft.png'></img> It's a draw! <img src='images/flagRight.png'></img>";
